38311eb666411717c6491b2ac5a8031565fa9349
Salah Moussaoui <60707540+moussaouisalah@users.noreply.github.com>
2021-05-16 14:11:45 +0100

    Update authProvider.ts

--- src/providers/authProvider.ts
+++ src/providers/authProvider.ts
@@ -2,19 +2,20 @@ import { auth, users } from "../temporaryData.json";
 
 const TOKEN_NAME = "auth_token";
 
-export const getIdentity = () => {
-  return new Promise((resolve, reject) => {
-    const token = localStorage.getItem(TOKEN_NAME);
-    if (!token) reject("Vous n'êtes pas connecté.");
-    const userAuth = auth.find((item) => item.token === token);
-    if (!userAuth) reject("invalid token");
-    const user = users.find((user) => user.id === userAuth!.userId);
-    resolve(user);
-  });
-};
-
-export const logout = () => {
-  localStorage.removeItem(TOKEN_NAME);
+export const authProvider = {
+  getIdentity: () => {
+    return new Promise((resolve, reject) => {
+      const token = localStorage.getItem(TOKEN_NAME);
+      if (!token) reject("Vous n'êtes pas connecté.");
+      const userAuth = auth.find((item) => item.token === token);
+      if (!userAuth) reject("invalid token");
+      const user = users.find((user) => user.id === userAuth!.userId);
+      resolve(user);
+    });
+  },
+  logout: () => {
+    localStorage.removeItem(TOKEN_NAME);
+  },
 };
 
 export const login = (email: string, password: string) => {};
