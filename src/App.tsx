8e5abc74def3e18b1bb236f17f004cd0fa143bc8
salah <60707540+moussaouisalah@users.noreply.github.com>
2021-05-17 20:32:49 +0100

    added json-server

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/sign-up/SignUp";
 import Index from "./pages/home/Index";
+import { userProvider } from "./providers/data-providers/userProvider";
 
 function App() {
+  userProvider.getList().then((data) => {
+    console.log(data);
+  });
   return (
     <Router>
       <Switch>
