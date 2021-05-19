2c48b61e1c348ffc79cb980b29fe1bf6bd6df3f2
salah <60707540+moussaouisalah@users.noreply.github.com>
2021-05-19 23:58:08 +0100

    added logout

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ## TODO
 
 - add sign up
-- add logout
 - add edit user
 - add change password
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import defaultImage from "../../assets/default-profile.jpg";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import { User } from "../../types/User";
+import { ClickAwayListener } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { authProvider } from "../../providers/authProvider";
 
 type NavbarProps = {
   user: User | undefined;
 };
 
 const Navbar = ({ user }: NavbarProps) => {
+  const [isDropDownEnabled, setDropDownEnabled] = useState(false);
+
+  const history = useHistory();
+
+  const handleRedirectToProfile = () => {
+    setDropDownEnabled(false);
+    user && history.push("/user/" + user.id);
+  };
+
+  const handleLogout = () => {
+    setDropDownEnabled(false);
+    authProvider.logout();
+    history.push("/login");
+  };
+
   return (
     <nav className="navbar">
       <div className="logo"></div>
@@ -27,10 +45,31 @@ const Navbar = ({ user }: NavbarProps) => {
               <div className="user-name">
                 {user.firstName} {user.lastName}
               </div>
-              <div className="user-dropdown">
+              <div
+                className="user-dropdown"
+                onClick={() => setDropDownEnabled(!isDropDownEnabled)}
+              >
                 <KeyboardArrowDownIcon />
               </div>
             </>
+          )}
+          {isDropDownEnabled && (
+            <ClickAwayListener onClickAway={() => setDropDownEnabled(false)}>
+              <div className="user-dropdown-modal">
+                <div
+                  className="user-dropdown-modal-item"
+                  onClick={handleRedirectToProfile}
+                >
+                  Profil
+                </div>
+                <div
+                  className="user-dropdown-modal-item"
+                  onClick={handleLogout}
+                >
+                  Se déconnecter
+                </div>
+              </div>
+            </ClickAwayListener>
           )}
         </div>
       </div>
