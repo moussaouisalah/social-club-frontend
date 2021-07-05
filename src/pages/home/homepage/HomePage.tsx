255a5fcda5305e9b38771a534ac0f4047c5a4899
Salah Moussaoui <60707540+moussaouisalah@users.noreply.github.com>
2021-07-05 21:43:51 +0100

    aze

--- src/pages/home/homepage/HomePage.tsx
+++ src/pages/home/homepage/HomePage.tsx
@@ -6,6 +6,8 @@ import { postProvider } from "../../../providers/data-providers/postProvider";
 import { userProvider } from "../../../providers/data-providers/userProvider";
 import { Post } from "../../../types/Post";
 import { User } from "../../../types/User";
+import banner1 from "../../../assets/banner.jpeg";
+import banner2 from "../../../assets/banner2.jpeg";
 
 type HomePageProps = {
   user: User | undefined;
@@ -34,26 +36,32 @@ const HomePage = ({ user }: HomePageProps) => {
   }, [user]);
 
   return (
-    <Content>
-      {posts.map((post, key) => (
-        <PostCard
-          key={key}
-          postId={post.id}
-          clubId={post.clubId}
-          userId={post.userId}
-          firstName={post.user?.firstName || ""}
-          lastName={post.user?.lastName || ""}
-          clubName={post.club?.name}
-          dateTime={post.creationDateTime}
-          profileImage={post.club?.profileImage || undefined}
-          color={post.club?.primaryColor}
-          text={post.text}
-          image={post.image}
-          likesCount={post.likesCount || 0}
-          commentsCount={post.commentsCount || 0}
-        />
-      ))}
-    </Content>
+    <div className="content-wrapper">
+      <Content>
+        {posts.map((post, key) => (
+          <PostCard
+            key={key}
+            postId={post.id}
+            clubId={post.clubId}
+            userId={post.userId}
+            firstName={post.user?.firstName || ""}
+            lastName={post.user?.lastName || ""}
+            clubName={post.club?.name}
+            dateTime={post.creationDateTime}
+            profileImage={post.club?.profileImage || undefined}
+            color={post.club?.primaryColor}
+            text={post.text}
+            image={post.image}
+            likesCount={post.likesCount || 0}
+            commentsCount={post.commentsCount || 0}
+          />
+        ))}
+      </Content>
+      <div className="side-banner">
+        <img className="banner" src={banner1} />
+        <img className="banner" src={banner2} />
+      </div>
+    </div>
   );
 };
 
